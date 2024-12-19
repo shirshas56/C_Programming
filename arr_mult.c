e6e82cbec58fd382b87f3d720410c31d58eb4973
shirshas56 <145319422+shirshas56@users.noreply.github.com>
2024-12-19 15:04:28 +0530

    Add files via upload

---
+++ arr_mult.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int row,col;
+    int i,j;
+    printf("Enter the no. of rows:\n");
+    scanf("%d",&row);
+    printf("Enter the no. of columns:\n");
+    scanf("%d",&col);
+
+    printf("Elements in the array:\n");
+        int arr[row][col];
+
+    for(i=0;i<row;i++){
+     for(j=0;j<col;j++){
+        printf("%d ",row,col);
+     }
+    }
+    printf("\n");
+
+return 0;
+}
