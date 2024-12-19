f215bf6b48fc3ac57dac25c3c4933cbc995b4636
shirshas56 <145319422+shirshas56@users.noreply.github.com>
2024-12-19 14:55:29 +0530

    Add files via upload

---
+++ perfect.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int num, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    if (num <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+    for (int i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+    if (sum == num) {
+        printf("%d is a perfect number.\n", num);
+    } else {
+        printf("%d is not a perfect number.\n", num);
+    }
+    return 0;
+}
